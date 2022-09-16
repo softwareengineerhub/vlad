c5ed66e2e5de69796d5378c86af3b101ef2e03c7
softwareengineerhub <denis.prokopiuk@yahoo.com>
2022-09-16 09:25:34 +0300

    Day20

---
+++ Day20/untitled/src/main/java/com/app02/Phone.java
@@ -0,0 +1,9 @@
+package com.app02;
+
+public class Phone {
+    public int price;
+
+    public String toString(){
+        return "price="+price;
+    }
+}
---
+++ Day20/untitled/src/main/java/com/app03/Smartphone.java
@@ -0,0 +1,13 @@
+package com.app03;
+
+public class Smartphone {
+    public int price;
+
+    public String toString(){
+        return "Smatphone="+price;
+    }
+
+    public int hashCode(){
+        return price;
+    }
+}
